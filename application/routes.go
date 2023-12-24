b55055dfcff6a42a656d96fce59e7118781c8be1
umuttopalak <umuttopalak@hotmail.com>
2023-12-24 15:16:31 +0300

    refactored code and routes

---
+++ application/routes.go
@@ -0,0 +1,32 @@
+package application
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/umuttopalak/orders-api/handler"
+)
+
+func loadRoutes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	router.Route("/order", loadOrderRoutes)
+
+	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/{id}", orderHandler.DeleteByID)
+}
