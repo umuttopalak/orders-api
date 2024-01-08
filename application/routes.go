6bf05642f6a29ddf7bd4ac8dc56bddd0e2dd462c
umuttopalak <umuttopalak@hotmail.com>
2024-01-08 09:58:22 +0300

    product route added

--- application/routes.go
+++ application/routes.go
@@ -8,6 +8,7 @@ import (
 	"github.com/umuttopalak/orders-api/handler"
 	"github.com/umuttopalak/orders-api/repository/customer"
 	"github.com/umuttopalak/orders-api/repository/order"
+	"github.com/umuttopalak/orders-api/repository/product"
 )
 
 func (a *App) loadRoutes() {
@@ -20,6 +21,7 @@ func (a *App) loadRoutes() {
 
 	router.Route("/customer", a.loadCustomerRoutes)
 	router.Route("/order", a.loadOrderRoutes)
+	router.Route("/product", a.loadProductRoutes)
 
 	a.router = router
 }
@@ -50,4 +52,18 @@ func (a *App) loadCustomerRoutes(router chi.Router) {
 	router.Get("/{id}", customerHandler.GetByID)
 	//router.Put("/{id}", customerHandler.UpdateByID)
 	router.Delete("/{id}", customerHandler.DeleteByID)
+}
+
+func (a *App) loadProductRoutes(router chi.Router) {
+	productHandler := &handler.Product{
+		Repo: &product.RedisRepo{
+			Client: a.rdb,
+		},
+	}
+	router.Post("/", productHandler.Create)
+	router.Get("/", productHandler.List)
+	router.Get("/{id}", productHandler.GetByID)
+	router.Put("/{id}", productHandler.UpdateByID)
+	router.Delete("/{id}", productHandler.DeleteByID)
+
 }
